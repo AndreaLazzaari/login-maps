63360922e56d185497ca7c79af8365d08a1083c2
Andrea Lazzari <andrea.lazzari8@gmail.com>
2024-09-24 09:50:13 +0200

    modifica app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Login from './models/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
